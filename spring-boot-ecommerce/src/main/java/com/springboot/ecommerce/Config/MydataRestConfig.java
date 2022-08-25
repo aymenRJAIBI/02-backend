c0bcc95a45a72f1353e41b0056ca6c9ad52a469e
aymenRJAIBI <aymenrjaibi23@gmail.com>
2022-08-25 11:37:20 +0200

    update task0003 : disable HTTP methods

--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/Config/MydataRestConfig.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/Config/MydataRestConfig.java
@@ -2,28 +2,31 @@ package com.springboot.ecommerce.Config;
 
 import com.springboot.ecommerce.entity.Product;
 import com.springboot.ecommerce.entity.ProductCategory;
-import org.springframework.data.repository.config.RepositoryConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
-public class MydataRestConfig implements RepositoryRestConfigurer {
+@Configuration
+class MyDataRestConfig implements RepositoryRestConfigurer {
 
-
-    @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
-        config.getExposureConfiguration()
-                .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+            HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
+
+            // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
+            config.getExposureConfiguration()
+                    .forDomainType(Product.class)
+                    .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                    .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+            // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
+            config.getExposureConfiguration()
+                    .forDomainType(ProductCategory.class)
+                    .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                    .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        }
     }
-}
+
