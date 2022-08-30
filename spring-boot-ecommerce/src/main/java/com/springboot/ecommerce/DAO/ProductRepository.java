2e633a7ab321425680d5ae215da5c85bba21fbeb
aymenRJAIBI <aymenrjaibi23@gmail.com>
2022-08-30 18:22:22 +0200

    add request findBycategoryId

--- spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/DAO/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/springboot/ecommerce/DAO/ProductRepository.java
@@ -1,10 +1,15 @@
 package com.springboot.ecommerce.DAO;
 
 import com.springboot.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "Products",path="product")
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    Page<Product> findByCategoryId(@Param("id")Long id, Pageable pageable);
 
 }
